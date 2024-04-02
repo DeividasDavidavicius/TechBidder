c316e5f0ccdb5ff237d92399f4f1bb642231c98b
DeividasDavidavicius <101116531+DeividasDavidavicius@users.noreply.github.com>
2024-04-03 00:15:10 +0300

    Added compatibility checking

--- Backend_PcAuction/Backend_PcAuction/Services/CalculationsService.cs
+++ Backend_PcAuction/Backend_PcAuction/Services/CalculationsService.cs
@@ -2,6 +2,7 @@
 using Backend_PcAuction.Data.Entities;
 using Backend_PcAuction.Data.Repositories;
 using Backend_PcAuction.Utils;
+using System.Runtime.CompilerServices;
 
 namespace Backend_PcAuction.Services
 {
@@ -58,32 +59,31 @@ namespace Backend_PcAuction.Services
 
         public async Task<List<Part>> GeneratePcBuild(PcBuilderDataDto pcBuilderDataDto)
         {
-            var motherboards = await GetMotherboards(pcBuilderDataDto.MotherboardId);
+            var motherboards = await GetParts(pcBuilderDataDto.MotherboardId, PartCategories.Motherboard);
             if (motherboards == null)
                 return null;
 
-            
+            var compatibleMotherboards = new List<Part>(motherboards);
+
             foreach(var motherboard in motherboards)
             {
-                CheckCompatibility(motherboard);
+                var result = await GenerateBuildForMotherboard(motherboard, pcBuilderDataDto);
+                if(result == -1)
+                {
+                    compatibleMotherboards.Remove(motherboard);
+                }
             }
             
-            /*
-            ParseData(pcBuilderDataDto.CpuId);
-            ParseData(pcBuilderDataDto.GpuId);
-            ParseData(pcBuilderDataDto.RamId);
-            ParseData(pcBuilderDataDto.SsdId);
-            ParseData(pcBuilderDataDto.HddId); */
-            //ParseData(pcBuilderDataDto.PsuId); // maybe only recommend PSU
+            // only recommend PSU
             var a = 123;
             return motherboards;
         }
 
-        public async Task<List<Part>> GetMotherboards(string id)
+        public async Task<List<Part>> GetParts(string id, string category)
         {
-            if(id.Equals("ANY"))
+            if (id.Equals("ANY"))
             {
-                return await _partsRepository.GetManyFromActiveAuctions(PartCategories.Motherboard);
+                return await _partsRepository.GetManyFromActiveAuctions(category);
             }
             else
             {
@@ -93,14 +93,98 @@ namespace Backend_PcAuction.Services
                     return null;
                 }
 
-                var part = await _partsRepository.GetFromActiveAuctions(PartCategories.Motherboard, guid);
+                var part = await _partsRepository.GetFromActiveAuctions(category, guid);
                 return new List<Part> { part };
             }
         }
 
-        public void CheckCompatibility(Part motherboard)
+        public async Task<int> GenerateBuildForMotherboard(Part motherboard, PcBuilderDataDto pcBuilderDataDto)
         {
-            var a = 123123;
+            var cpuList = pcBuilderDataDto.CpuId == null ? null : await GetParts(pcBuilderDataDto.CpuId, PartCategories.CPU);
+            var gpuList = pcBuilderDataDto.GpuId == null ? null : await GetParts(pcBuilderDataDto.GpuId, PartCategories.GPU);
+            var ramList = pcBuilderDataDto.RamId == null ? null : await GetParts(pcBuilderDataDto.RamId, PartCategories.RAM);
+            var ssdList = pcBuilderDataDto.SsdId == null ? null : await GetParts(pcBuilderDataDto.SsdId, PartCategories.SSD);
+            var hddList = pcBuilderDataDto.HddId == null ? null : await GetParts(pcBuilderDataDto.HddId, PartCategories.HDD);
+
+            if(cpuList != null)
+            {
+                cpuList = CheckCpuCompatibility(motherboard, cpuList);
+                if (cpuList == null) return -1;
+            }
+
+            if (ramList != null) 
+            {
+                ramList = CheckRamCompatibility(motherboard, ramList);
+                if(ramList == null) return -1;
+            }
+
+            if(ssdList != null)
+            {
+                ssdList = CheckSsdCompatibility(motherboard, ssdList);
+                if (ssdList == null) return -1;
+            }
+
+            if (hddList != null)
+            {
+                hddList = CheckSsdCompatibility(motherboard, hddList);
+                if (hddList == null) return -1;
+            }
+
+            return 0;
+        }
+
+        public List<Part> CheckCpuCompatibility(Part motherboard, List<Part> cpuList)
+        {
+            cpuList = cpuList.Where(cpu => cpu.SpecificationValue1.Equals(motherboard.SpecificationValue1)).ToList();
+
+            if (cpuList.Count == 0)
+            {
+                return null;
+            }
+
+            return cpuList;
+        }
+
+        public List<Part> CheckRamCompatibility(Part motherboard, List<Part> ramList)
+        {
+            ramList = ramList.Where(ram =>
+                ram.SpecificationValue3.Equals(motherboard.SpecificationValue2) &&
+                Double.Parse(ram.SpecificationValue1) <= Double.Parse(motherboard.SpecificationValue4) &&
+                Double.Parse(ram.SpecificationValue4) <= Double.Parse(motherboard.SpecificationValue3)
+            ).ToList();
+
+            if (ramList.Count == 0)
+            {
+                return null;
+            }
+
+            return ramList;
+        }
+
+        public List<Part> CheckSsdCompatibility(Part motherboard, List<Part> ssdList)
+        {
+            ssdList = ssdList.Where(ssd =>
+                ((ssd.SpecificationValue2 != "" && (motherboard.SpecificationValue5 != "" || motherboard.SpecificationValue7 != "")) || (ssd.SpecificationValue3 != "" && motherboard.SpecificationValue9 != ""))
+            ).ToList();
+
+            if (ssdList.Count == 0)
+            {
+                return null;
+            }
+
+            return ssdList;
+        }
+
+        public List<Part> CheckHddCompatibility(Part motherboard, List<Part> hddList)
+        {
+            hddList = hddList.Where(hdd => Double.Parse(motherboard.SpecificationValue9) > 0).ToList();
+
+            if (hddList.Count == 0)
+            {
+                return null;
+            }
+
+            return hddList;
         }
     }
 }
